ebb794df8cf2598aa1ce9ab8111c64da4b681735
Armaghan-Mardasi <perfect.princess929292@gmail.com>
2023-09-10 22:13:10 +0330

    session 3

---
+++ session 3/main.ts
@@ -0,0 +1,63 @@
+//exercise 1
+// let userInput = 18;
+
+// if (userInput < 20) {
+//   console.log("you are under 20");
+// } else {
+//   console.log("welcome");
+// }
+
+//exercise 2
+// let userInput: number = 65;
+
+// if (userInput >= 90) {
+//   console.log("A");
+// } else if (userInput >= 80) {
+//   console.log("B");
+// } else if (userInput >= 70) {
+//   console.log("C");
+// } else if (userInput >= 60) {
+//   console.log("D");
+// } else if (userInput >= 50) {
+//   console.log("E");
+// } else {
+//   console.log("F");
+// }
+
+//exercise 3
+// let currentColor = "yellow";
+// let nextColor = "green";
+
+// if (currentColor === "red") {
+//   nextColor = "green";
+// } else if (currentColor === "yellow") {
+//   nextColor = "red";
+// } else if (currentColor === "green") {
+//   nextColor = "yellow";
+// }
+
+// console.log(nextColor);
+
+//exercise 4
+// let day: number = 3;
+
+// switch (day) {
+//   case 1:
+//     console.log("shanbe");
+//     break;
+
+//   case 2:
+//     console.log("yekshanbe");
+//     break;
+//   case 3:
+//     console.log("seshanbe");
+//     break;
+//   case 4:
+//     console.log("chaharshanbe");
+//     break;
+//   case 5:
+//     console.log("panjshanbe");
+//     break;
+//   case 6:
+//     console.log("jome");
+// }
