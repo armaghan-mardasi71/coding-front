58c80f7e39e3c18d8ba843d33ba48e035bd41cc8
Armaghan-Mardasi <perfect.princess929292@gmail.com>
2023-09-18 22:37:01 +0330

    edited types of S.4

--- session 4/main.ts
+++ session 4/main.ts
@@ -7,44 +7,84 @@
 // console.log(a);
 // -------------------------------------------
 // ex 2:
-// let userBasket = ["pen"];
-// function addToCart(product: string) {
-//   userBasket.push(product);
+// interface Product {
+//   name: string;
+//   count: number;
 // }
-// addToCart("book");
-// addToCart("phone");
+
+// let userBasket: Product[] = [];
+// let userInput;
+// let userInput2: number;
+// while (userInput !== "end") {
+//   userInput = prompt("enter product name:");
+
+//   if (userInput !== "end") {
+//     userInput2 = Number(prompt("enter product count:"));
+//     if (userInput && userInput2) {
+//       addToCart(userInput, userInput2);
+//     }
+//   }
+// }
+
+// function addToCart(userInput: string, userInput2: number) {
+//   let newProduct = {
+//     id: userBasket.length + 1,
+//     name: userInput,
+//     count: userInput2
+//   };
+//   userBasket.push(newProduct);
+// }
 // console.log(userBasket);
+
 // -------------------------------------------
 
 // ex 3:
+// interface Contacts {
+//   name: string;
+//   phone: number;
+// }
+// let contacts: Contacts[] = [];
+// let contactCount = Number(prompt("how many contacts do you want to add?"));
+// let contactName;
+// let contactPhone: number;
+// for (let i = 0; i < contactCount; i++) {
+//   contactName = prompt("enter the name:");
+//   contactPhone = Number(prompt("enter the phone number:"));
+//   let newContact = {
+//     id: contacts.length + 1,
+//     name: contactName,
+//     phone: contactPhone
+//   };
+//   contacts.push(newContact);
+// }
+// console.log(contacts);
+//---------------------------------------------
 
-// let contacts = [
+// ex 4:
+// interface Find {
+//   id: number;
+//   name: string;
+//   number: number;
+// }
+// let contacts: Find[] = [
 //   { id: 1, name: "armaghan", number: 9309665106 },
 //   { id: 2, name: "sara", number: 9124562123 },
 //   { id: 3, name: "hadis", number: 9365469878 }
 // ];
-// console.log(contacts[1]);
-//---------------------------------------------
 
-// ex 4:
-let contacts = [
-  { id: 1, name: "armaghan", number: 9309665106 },
-  { id: 2, name: "sara", number: 9124562123 },
-  { id: 3, name: "hadis", number: 9365469878 }
-];
-let userInput = prompt("enter your name:");
-if (userInput) {
-  findContact(userInput);
-}
+// let userInput = prompt("enter your name:");
+// if (userInput) {
+//   findContact(userInput);
+// }
 
-function findContact(contactName: string) {
-  let mainContact = contacts.some((person) => {
-    return person.name === contactName;
-  });
+// function findContact(contactName: string) {
+//   let mainContact: boolean = contacts.some((person) => {
+//     return person.name === contactName;
+//   });
 
-  if (mainContact) {
-    console.log("contact is available");
-  } else {
-    console.log("Sorry,contact is NOT available");
-  }
-}
+//   if (mainContact) {
+//     console.log("contact is available");
+//   } else {
+//     console.log("Sorry,contact is NOT available");
+//   }
+// }
